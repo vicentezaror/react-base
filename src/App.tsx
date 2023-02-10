b2768acd5fb0f4575d708846ddec13a1ea4eecca
Vicente Zaror <vicente@kydor.net>
2023-02-09 23:28:10 -0300

    update App.tsx to clear page

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,7 @@
-import Navbar from './components/Navbar/Navbar';
-import Title from './components/Title/Title';
-import Done from './components/Icon/AnimatedIcon/done.json';
-import NavItem from './components/Navbar/NavItem';
-import HomeIcon from './components/Icon/FlatIcon/HomeIcon';
-import PuzzleIcon from './components/Icon/FlatIcon/PuzzleIcon';
-import ProfileIcon from './components/Icon/FlatIcon/ProfileIcon';
-import MailboxIcon from './components/Icon/FlatIcon/MailboxIcon';
-
 function App() {
   return (
-    <main className='flex flex-col place-content-evenly bg-gradient-to-b from-primary-lighter to-primary-dark w-screen h-screen'>
-      <Title text='Kydor' />
-      <Navbar>
-        <NavItem
-          title='Home'
-          icon={
-            <HomeIcon width='48px' height='48px' />
-          }
-        />
-        <NavItem
-          title='Projects'
-          icon={
-            <PuzzleIcon width='48px' height='48px' />
-          }
-        />
-        <NavItem
-          title='Blog'
-          icon={
-            <MailboxIcon width='48px' height='48px' />
-          }
-        />
-        <NavItem
-          title='Profile'
-          icon={
-            <ProfileIcon width='48px' height='48px' />
-          }
-        />
-      </Navbar>
+    <main className='w-screen h-screen'>
+      <p>React Base Project</p>
     </main>
   );
 }
